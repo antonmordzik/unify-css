39b116d0db7a4233a046cca304ffb27683cc54d2
Anton Mordik <amordik@elinext.com>
2020-06-07 22:53:08 +0300

    project structure init

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import fs from 'fs';
+import path from 'path';
+import chalk from 'chalk';
+
+const cli = new Command();
+
+cli.version('0.0.1');
+console.log(chalk.red('Unify CSS started'));
+
+cli
+  .option('-C, --config-path', 'path to .uniconfig')
+  .option('-S, --source', 'path to source dir')
+  .option('-O, --output', 'path to output dir')
+  .option('-P, --plugin', 'path to js plugin file')
+  .parse(process.argv);
+
+console.log('config-path: ', cli.configPath);
+console.log('source: ', cli.source);
+console.log('output: ', cli.output);
+console.log('plugin: ', cli.plugin);
