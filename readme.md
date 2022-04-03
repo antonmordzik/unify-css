2efb44f6775fc2fd09994c228d0425ec6607bf21
Anton Mordik <mordikk@gmail.com>
2022-04-04 00:34:30 +0200

    chore: Package deps update

--- readme.md
+++ readme.md
@@ -1,8 +1,36 @@
-# Unify-css
-Example of '.uniconfig'
-```json
-{
-    "sources": ["./css/*.css", "./**/*.css"],
-    "output": "./dist/"
+# Unify CSS
+
+*The repository was created solely for research purposes. Perhaps in the future, if the theory is confirmed, it might be transformed into a webpack plugin.*
+
+The idea of the postprocessor is to test css optimizing, making each unique css rule be appeared only once.
+
+For example, for given input file
+```css
+.a {
+  box-sizing: border-box;
+  color: #ff0000;
+  border: thin solid #ff0000;
+}
+
+.b {
+  box-sizing: border-box;
+  color: #00ff00;
+  border: thin solid #ff0000;
 }
 ```
+css will be transaformed into
+```css
+.a, .b {
+  box-sizing: border-box;
+  border: thin solid #ff0000;
+}
+
+.a {
+  color: #ff0000;
+}
+
+.b {
+  color: #00ff00;
+}
+```
+On small classes with not a lot of rules it looks unoptimized, but I believe, that on larger ones it will bring more performance.
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,32 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { program } from 'commander';
 import fs from 'fs';
 import path from 'path';
-import chalk from 'chalk';
 import { resolvePaths } from './utils/resolver';
 import { readFiles } from './utils/reader';
 import { process as parseProcess, IDeclaration } from './utils/parser';
-const cli = new Command();
-const failText = 'Running unify-css ' + chalk.red('failed');
 
-cli.version('0.0.1');
-console.log(chalk.green('Unify CSS started'));
+const failText = 'Running unify-css falied.';
 
-cli
+program.version('0.0.1');
+console.log('Unify CSS started');
+
+program
   .option('-C, --config-path <path>', 'path to .uniconfig')
   .option('-S, --source <path>', 'path to source dir')
   .option('-O, --output <path>', 'path to output dir')
   .option('-P, --plugin <path>', 'path to js plugin file')
   .parse(process.argv);
 
-const configPath = path.resolve(process.cwd(), cli.configPath || './', '.uniconfig')
+const options = program.opts<{
+  configPath: string;
+  source: string;
+  output: string;
+  plugin: string;
+}>()
+
+const configPath = path.resolve(process.cwd(), options.configPath || './', '.uniconfig');
 interface IConfig {
   sources: string[];
   output: string;
@@ -30,14 +36,14 @@ try {
   const configFile = fs.readFileSync(configPath).toString();
   config = JSON.parse(configFile);
 } catch (jsonParseError) {
-  console.log(chalk.red(`Cannot read '${configPath}'`));
+  console.log(`Cannot read '${configPath}'`);
   console.log(failText);
   process.exitCode = 1;
   process.exit();
 }
 
 if (!Array.isArray(config.sources) || !config.sources.every(source => typeof source === 'string')) {
-  console.log(chalk.red('Incorrect format of sources.') + ' Use string[] notation.')
+  console.log('Incorrect format of sources. Use string[] notation.')
   console.log(failText);
   process.exitCode = 1;
   process.exit();
@@ -49,4 +55,4 @@ const cssPaths = resolvePaths(sources);
 
 const cssFiles = readFiles(cssPaths);
 
-const declarations: IDeclaration[][] = cssFiles.map(file => parseProcess(file))
+const declarations: IDeclaration[][] = cssFiles.map(file => parseProcess(file));
